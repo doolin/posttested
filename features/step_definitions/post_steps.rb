ed95e51858673530f4c4f528efa750ce7a7ab56e
Dave Doolin <david.doolin@gmail.com>
2012-12-15 13:09:34 -0800

    updated for new gemset

---
+++ features/step_definitions/post_steps.rb
@@ -0,0 +1,13 @@
+Given /^current user is signed in$/ do  pending # express the regexp above with the code you wish you had
+end
+
+Given /^is on the post creation page$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^the user publishes a post$/ do
+    pending # express the regexp above with the code you wish you had
+end
+Then /^the user is shown the new post$/ do
+    pending # express the regexp above with the code you wish you had
+end
